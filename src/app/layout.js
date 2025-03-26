b35b59a94829857ca5cec1733bedee59135dd25b
Abu-Sayed-Sarker <abusayed96728@gmail.com>
2025-03-26 23:39:05 +0600

    Footer section done without Link and logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "./components/Footer/Footer";
+import Footer from "@/components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
