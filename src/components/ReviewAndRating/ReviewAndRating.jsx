0386c77408b429065a130131c0346ed1d0705ff7
Ahanaf Mubasshir <ahanaf607307@gmail.com>
2025-03-27 21:47:07 +0600

    fix packeg json related error

--- src/components/ReviewAndRating/ReviewAndRating.jsx
+++ src/components/ReviewAndRating/ReviewAndRating.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const ReviewAndRating = () => {
     return (
         <div>
-            Review And Rating start
+            
+            <h1 className='text-5xl'>Review And Rating start</h1>
+            <h1 className='text-sm'>Review And Rating ends</h1>
         </div>
     );
 };
