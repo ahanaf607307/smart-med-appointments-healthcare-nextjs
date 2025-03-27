f044ee1a9b440040bbf5613ac83e4305f838077b
Takmim00 <takmimm@gmail.com>
2025-03-27 19:42:35 +0600

    why chooseuse added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Banner/Banner";
 import NewsLetter from "@/components/NewsLetter/NewsLetter";
+import WhyChooseUs from "@/components/WhyChooseUs/WhyChooseUs";
 
 export default function Home() {
   return (
     <div >
      <Banner/>
+     <WhyChooseUs/>
      <NewsLetter/>
     </div>
   );
