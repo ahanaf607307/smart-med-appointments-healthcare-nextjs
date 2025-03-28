6026c54804aa618cde0b23f2522c9c5bebe9768a
Ahanaf Mubasshir <ahanaf607307@gmail.com>
2025-03-28 11:38:52 +0600

    change review section

--- src/components/ReviewAndRating/ReviewAndRating.jsx
+++ src/components/ReviewAndRating/ReviewAndRating.jsx
@@ -4,9 +4,9 @@ const ReviewAndRating = () => {
     return (
         <div>
             
-            <h1 className='text-5xl'>Review And Rating start</h1>
+            {/* <h1 className='text-5xl'>Review And Rating start</h1>
             <h1 className='text-sm'>Review And Rating ends</h1>
-            <p className='text-sm'>Review And Rating ends</p>
+            <p className='text-sm'>Review And Rating ends</p> */}
         </div>
     );
 };
