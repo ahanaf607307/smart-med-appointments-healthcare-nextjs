588cff5075a7595eaed5f1cbae1c264f8d4b6b6c
Takmim00 <takmimm@gmail.com>
2025-03-29 18:44:26 +0600

    login and register user show

---
+++ src/app/(app)/actions/auth/loginUser.js
@@ -0,0 +1,21 @@
+"use server";
+
+import bcrypt from "bcrypt"
+
+
+export const loginUser = async (payload) => {
+
+    const { email, password } = payload;
+
+    // TO DO 
+
+
+    
+    // const user = {}
+
+    // if (!user) return null
+    // const isPasswordOK = bcrypt.compare(user.password, password)
+    // if (!isPasswordOK) return null
+
+    return { email, password };
+}
