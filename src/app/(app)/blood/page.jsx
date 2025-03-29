ebb1b6d307e249b85b8334042ff135dad0b15206
Ahanaf Mubasshir <ahanaf607307@gmail.com>
2025-03-29 22:26:19 +0600

    compleate blood donation form

--- src/app/(app)/blood/page.jsx
+++ src/app/(app)/blood/page.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Blood = () => {
     return (
-        <div  className='container mx-auto'>
-    <div className='py-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:gap-10'>
+        <div  className='container mx-auto p-4'>
+    <div className='py-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5 lg:gap-10'>
     <BloodCard/>
     <BloodCard/>
     <BloodCard/>
