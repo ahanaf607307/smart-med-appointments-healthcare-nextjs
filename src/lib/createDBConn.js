7e71dbd69563fab3613c7d51764157aa45c28571
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-03-30 09:54:55 +0600

    Add mongoose for db connection.

---
+++ src/lib/createDBConn.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const createDBConn = () => {
+  try {
+    const DB_USERNAME = process.env.DB_USERNAME;
+    const DB_PASSWORD = process.env.DB_PASSWORD;
+    const MONGO_URI = process.env.MONGO_URI.replace(
+      "<db_username>",
+      DB_USERNAME
+    ).replace("<db_password>", DB_PASSWORD);
+
+    mongoose.connect(MONGO_URI);
+  } catch (err) {
+    console.log(err);
+  }
+};
