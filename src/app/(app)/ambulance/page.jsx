ac780d474c3c56543c4e67bdf2994faf9fe22308
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-04-02 16:22:14 +0600

    Fetch all ambulances.

--- src/app/(app)/ambulance/page.jsx
+++ src/app/(app)/ambulance/page.jsx
@@ -1,21 +1,34 @@
-import AmbulanceCard from '@/components/AmbulanceCard/AmbulanceCard';
-import React from 'react';
+import AmbulanceCard from "@/components/AmbulanceCard/AmbulanceCard";
+import React from "react";
 
-const AmbulanceService = () => {
-    return (
-        <div className='p-5'>
-            <h1 className='text-5xl text-center pt-10 font-bold text-cyan-700'>Ambulance Service</h1>
-           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-20'>
-           <AmbulanceCard/>
-           <AmbulanceCard/>
-           <AmbulanceCard/>
-           <AmbulanceCard/>
-           <AmbulanceCard/>
-           <AmbulanceCard/>
-           <AmbulanceCard/>
-           </div>
-        </div>
-    );
+const AmbulanceService = async () => {
+  const res = await fetch("http://localhost:3000/api/getAllAmbulances", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const ambulances = await res.json();
+
+  // Map ambulances array and show all the information in card.
+  console.log(ambulances);
+
+  return (
+    <div className="p-5">
+      <h1 className="text-5xl text-center pt-10 font-bold text-cyan-700">
+        Ambulance Service
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-20">
+        <AmbulanceCard />
+        <AmbulanceCard />
+        <AmbulanceCard />
+        <AmbulanceCard />
+        <AmbulanceCard />
+        <AmbulanceCard />
+        <AmbulanceCard />
+      </div>
+    </div>
+  );
 };
 
 export default AmbulanceService;
