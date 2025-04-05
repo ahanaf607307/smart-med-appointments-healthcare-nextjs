d71e279cd69aafba7f521a76beaf5ed0cf1e18d2
Abu-Sayed-Sarker <abusayed96728@gmail.com>
2025-04-05 11:31:15 +0600

    Single doctor information required for appointments and doctor details page

--- src/app/(app)/alldoctor/[id]/page.jsx
+++ src/app/(app)/alldoctor/[id]/page.jsx
@@ -1,13 +1,18 @@
 import AppointmentForm from "./components/Appointment-from";
 
-const page = () => {
+const page = ({ params }) => {
+
+    const { id } = params
+    console.log(id);
+
+
     return (
         <div className="max-w-11/12 mx-auto mt-10">
             <div className="text-center mb-8">
                 <h1 className="text-5xl font-bold mb-2 uppercase">Doctor <span className='text-[#00BDE0]'>Appointment</span></h1>
             </div>
             <div className="mb-10">
-                <AppointmentForm></AppointmentForm>
+                <AppointmentForm id={id}></AppointmentForm>
             </div>
         </div>
     );
